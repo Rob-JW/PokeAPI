d73ed6d0cb374698e5e574252877fa41a45ce3d6
Glitch (glitch-hello-node) <none>
2023-03-31 21:06:16 +0000

    ☂️👱 Checkpoint ./index.js:56579652/169

--- index.js
+++ index.js
@@ -6,5 +6,11 @@ const notion = new Client ({auth: process.env.NOTION_KEY})
 const pakeArray = []
 
 async function getPokemaon() {
-  
+  await axios.get('https://pokeapi.co/api/v2/pokemon/1')
+    .then((poke) => {
+      console.log(poke)
+  })
+    .catch((error) => {
+      console.log(error)
+  })
 }
