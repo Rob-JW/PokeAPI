1d060a034c0238b7984176a476ea12ab888dd0fe
Glitch (glitch-hello-node) <none>
2023-03-31 20:55:14 +0000

    🌌🚤 Checkpoint ./index.js:56579652/162

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-const axios = require("axios")
-const { client } = require()
+const axios = require('axios')
+const { Client } = require('@notionhq/client')
+
+const notion = new Client ({auth: process.env.NOTION_KEY})
+
+const pakeArray = []
+
+async function getPokemaon() {
+  
+}
