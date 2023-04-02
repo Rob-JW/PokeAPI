7443878b032f070c459d9663d50a7d07d456c1b5
Glitch (glitch-hello-node) <none>
2023-04-02 15:44:58 +0000

    🕷🏬 Checkpoint ./index.js:56579652/1443

--- index.js
+++ index.js
@@ -6,11 +6,14 @@ const notion = new Client ({auth: process.env.NOTION_KEY})
 const pakeArray = []
 
 async function getPokemaon() {
-  await axios.get('https://pokeapi.co/api/v2/pokemon/1')
-    .then((poke) => {
-      console.log(poke)
-  })
-    .catch((error) => {
-      console.log(error)
-  })
+  await axios.get('https://pokeapi.co/api/v2/pokemon/1').
 }
+
+  const start = 1
+  const end = 10
+  for (let i = start; <= end; i++){
+    const poke = 
+      .then((poke) => {
+        console.log(poke)
+        .catch((error) => {
+        console.log(error)
